2149452ed96e55609d2151478ac7809b4ea76806
tangfeixiong <tangfx128@hotmail.com>
2015-02-09 01:49:09 +0800

    15-2-9 by tangfx

---
+++ project/src/main/java/com/chinapower/jopenstack/jopenstack_project/App.java
@@ -0,0 +1,13 @@
+package com.chinapower.jopenstack.jopenstack_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
