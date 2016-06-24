c20bca8339d4dffb8a5b882c3ed6878c9faaeb48
樊红灵 <fanhongling@fanhonglingdeMacBook-Pro.local>
2016-06-24 12:39:35 -0700

    logstash appender

--- qingyuanos-caas-delivery-example/src/main/java/com/qingyuanos/delivery/example/Slf4jWithLog4jAndLogstash.java
+++ qingyuanos-caas-delivery-example/src/main/java/com/qingyuanos/delivery/example/Slf4jWithLog4jAndLogstash.java
@@ -3,6 +3,7 @@ package com.qingyuanos.delivery.example;
 import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class Slf4jWithLog4jAndLogstash {
 
@@ -11,6 +12,11 @@ public class Slf4jWithLog4jAndLogstash {
 		PropertyConfigurator.configure(Slf4jWithLog4jAndLogstash.class.getClassLoader().getResource("com/qingyuanos/delivery/example/log4j.properties"));
 		//PropertyConfigurator.configure(this.getClass().getClassLoader().getResource("./log4j.properties"));
 
+        Object obj = System.getenv("APP_ENV");
+		if (obj != null) {
+		    MDC.put("environment", System.getenv("APP_ENV"));
+		}
+		
 		Logger logger = LoggerFactory.getLogger(Slf4jWithLog4jAndLogstash.class);
 		logger.debug("Hello World!");
 		logger.info("info");
